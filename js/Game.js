709c8d57e7bc0301f95cfe7b1a6ed835e309332d
RyanLuttrell <ryan.m.luttrell@gmail.com>
2020-07-20 08:42:12 -0600

    Building out the class and construcors

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,19 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+    constructor() {
+        this.missed = 0;
+        this.phrases = createPhrases();
+        this.activePhrase = null;
+    }
+
+    /**
+     * Creates phrases for use in the game
+     * @return {array} an array of phrases that could be used in the game
+     */
+    createPhrases() {
+
+    };
+}
