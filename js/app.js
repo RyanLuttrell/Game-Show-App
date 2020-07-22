3cbeeb660e7e2d77bcf88160fdd4e746f6e3ecc1
RyanLuttrell <ryan.m.luttrell@gmail.com>
2020-07-22 08:37:55 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,6 +2,11 @@ const game = new Game();
 const startButton = document.querySelector('#btn__reset');
 startButton.addEventListener('click', () => {
     const letters = document.querySelectorAll('ul li').forEach(letter => letter.remove());
+    const keys = document.querySelectorAll('.key');
+    for (let i = 0; i < keys.length; i++) {
+        keys[i].disabled = '';
+        keys[i].className = 'key';
+    }
     game.startGame();
     console.log(`Active Phrase - phrase: ${game.activePhrase.phrase}`)
 })
